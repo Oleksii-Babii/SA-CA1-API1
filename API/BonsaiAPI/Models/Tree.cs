69999165900b52b69c7ddd05d41816b81c765f8f
Oleksii-Babii <babiy.olexiy@ukr.net>
2026-04-18 09:31:07 +0100

    feat(models): add Tree entity with nickname, age and Species navigation

---
+++ API/BonsaiAPI/Models/Tree.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BonsaiAPI.Models
+{
+    public class Tree
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nickname { get; set; } = string.Empty;
+
+        [Range(0, 5000)]
+        public int Age { get; set; }
+
+        [Range(typeof(decimal), "0", "10000")]
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Height { get; set; }
+
+        public DateTime LastWateredDate { get; set; }
+
+        public string? Notes { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        [ForeignKey("Species")]
+        public int SpeciesId { get; set; }
+
+        public Species? Species { get; set; }
+    }
+}
