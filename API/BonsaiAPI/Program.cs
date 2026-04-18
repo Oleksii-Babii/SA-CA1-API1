2a7364c622aa4a71ca012252a3d828856e09c8a5
ihor.tryndey <igortryndey@gmail.com>
2026-04-18 23:52:16 +0100

    Register BonsaiContext with the SQL Server connection string

--- API/BonsaiAPI/Program.cs
+++ API/BonsaiAPI/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using BonsaiAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BonsaiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BonsaiContext")
+        ?? throw new InvalidOperationException("Connection string 'BonsaiContext' not found.")));
 
 var app = builder.Build();
 
