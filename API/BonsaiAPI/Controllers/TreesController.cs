9e2a63c7e79d98fca55c3cb594408476c55d870f
ihor.tryndey <igortryndey@gmail.com>
2026-04-22 17:29:48 +0100

    Scaffold TreesController with list and get-by-id endpoints

---
+++ API/BonsaiAPI/Controllers/TreesController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BonsaiAPI.Data;
+using BonsaiAPI.Models;
+
+namespace BonsaiAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreesController : ControllerBase
+    {
+        private readonly BonsaiContext _context;
+
+        public TreesController(BonsaiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/trees
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Tree>>> GetAllTrees()
+        {
+            return await _context.Trees
+                .OrderBy(t => t.Nickname)
+                .ToListAsync();
+        }
+
+        // GET: api/trees/1
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Tree>> GetTree([FromRoute] int id)
+        {
+            var tree = await _context.Trees.FirstOrDefaultAsync(t => t.Id == id);
+            if (tree == null)
+            {
+                return NotFound();
+            }
+            return Ok(tree);
+        }
+    }
+}
