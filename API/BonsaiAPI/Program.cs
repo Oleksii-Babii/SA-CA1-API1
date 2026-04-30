785122ab6019b590ddc241c8ef09491eae90920c
ihor.tryndey <igortryndey@gmail.com>
2026-04-30 09:13:08 +0100

    Serve wwwroot files and ensure uploads/ directory exists at boot

--- API/BonsaiAPI/Program.cs
+++ API/BonsaiAPI/Program.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 using BonsaiAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseWebRoot("wwwroot");
 
 builder.Services.AddDbContext<BonsaiContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("BonsaiContext")
@@ -24,6 +25,10 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+var webRootPath = app.Environment.WebRootPath ?? Path.Combine(app.Environment.ContentRootPath, "wwwroot");
+Directory.CreateDirectory(webRootPath);
+Directory.CreateDirectory(Path.Combine(webRootPath, "uploads"));
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<BonsaiContext>();
@@ -32,6 +37,8 @@ using (var scope = app.Services.CreateScope())
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseStaticFiles();
 
 app.UseCors();
 
